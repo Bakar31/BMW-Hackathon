44f319da4bb3981db12a190e45bf99432e62ad7a
Bakar31 <abubakar1808031@gmail.com>
2022-12-15 14:30:55 +0600

    draw box from json

---
+++ src/json_to_txt.py
@@ -0,0 +1,30 @@
+import json
+import cv2
+
+with open('objectclasses.json') as f:
+        objects = json.load(f)
+
+class_dict = {}
+for classes in objects:
+    class_dict[classes['Id']] = classes['Name']
+# print(class_dict)
+
+
+with open('labels/51.json') as f:
+    objects_classes = json.load(f)
+
+img = cv2.imread('images/51.jpg')
+# with open('test.txt', 'w') as text_file:
+for objects in objects_classes:
+    class_id = objects['ObjectClassId']
+    x1 = objects['Left']
+    y1 = objects['Top']
+    x2 = objects['Right']
+    y2 = objects['Bottom']
+    print(class_id, x1, y1, x2, y2)
+    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 0))
+    
+
+
+cv2.imshow('box', img)
+cv2.waitKey(0)
